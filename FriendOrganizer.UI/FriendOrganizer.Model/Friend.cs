8f9761636924c513b47651d3e4dce7a272b2325b
s3blag <226173@student.pwr.edu.pl>
2018-03-22 23:17:52 +0100

    Set up the FriendOrganizer Solution

---
+++ FriendOrganizer.UI/FriendOrganizer.Model/Friend.cs
@@ -0,0 +1,14 @@
+﻿namespace FriendOrganizer.Model
+{
+    public class Friend
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public int LastName { get; set; }
+
+        public int Email { get; set; }
+        
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # FriendOrganizer
+WPF, MVVM, Entity Framework app based on a pluralsight course.
