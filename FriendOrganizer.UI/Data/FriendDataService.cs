775ca05b909388a35c8a20d509b53b29c72d5567
s3blag <226173@student.pwr.edu.pl>
2018-03-25 20:24:53 +0200

    Add basic UI Layer with DI

---
+++ FriendOrganizer.UI/Data/FriendDataService.cs
@@ -0,0 +1,16 @@
+﻿using FriendOrganizer.Model;
+using System.Collections.Generic;
+
+namespace FriendOrganizer.UI.Data
+{
+    public class FriendDataService : IFriendDataService
+    {
+        public IEnumerable<Friend> GetAll()
+        {
+            yield return new Friend { FirstName = "Tomasz", LastName = "Huber" };
+            yield return new Friend { FirstName = "Andrzej", LastName = "Konieczny" };
+            yield return new Friend { FirstName = "Julia", LastName = "Konieczna" };
+            yield return new Friend { FirstName = "Marcelina", LastName = "Huber" };
+        }
+    }
+}
