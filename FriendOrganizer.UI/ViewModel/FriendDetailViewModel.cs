8238a47f455444672c762485cf74ecbef098fa15
s3blag <226173@student.pwr.edu.pl>
2018-04-10 12:42:58 +0200

    Add saving data

--- FriendOrganizer.UI/ViewModel/FriendDetailViewModel.cs
+++ FriendOrganizer.UI/ViewModel/FriendDetailViewModel.cs
@@ -1,9 +1,11 @@
 ﻿using FriendOrganizer.Model;
 using FriendOrganizer.UI.Data;
 using FriendOrganizer.UI.Event;
+using Prism.Commands;
 using Prism.Events;
 using System;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace FriendOrganizer.UI.ViewModel
 {
@@ -12,6 +14,18 @@ namespace FriendOrganizer.UI.ViewModel
         private IFriendDataService _dataService;
         private IEventAggregator _eventAggregator;
         private Friend _friend;
+    
+        public Friend Friend
+        {
+            get { return _friend; }
+            private set
+            {
+                _friend = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand SaveCommand { get; }
 
         public FriendDetailViewModel(IFriendDataService dataService,
             IEventAggregator eventAggregator)
@@ -20,6 +34,25 @@ namespace FriendOrganizer.UI.ViewModel
             _eventAggregator = eventAggregator;
             _eventAggregator.GetEvent<OpenFriendDetailViewEvent>()
                 .Subscribe(OnOpenFriendDetailViewAsync);
+
+            SaveCommand = new DelegateCommand(OnSaveExecute, OnSaveCanExecute);
+        }
+
+        private async void OnSaveExecute()
+        {
+            await _dataService.SaveAsync(Friend);
+            _eventAggregator.GetEvent<AfterFriendSavedEvent>().Publish(
+                new AfterFriendSavedEventArgs
+                {
+                    Id = Friend.Id,
+                    DisplayMember = $"{Friend.FirstName} {Friend.LastName}"
+                });
+        }
+
+        private bool OnSaveCanExecute()
+        {
+            // TODO: check if friend is valid
+            return true;
         }
 
         private async void OnOpenFriendDetailViewAsync(int friendId)
@@ -32,15 +65,6 @@ namespace FriendOrganizer.UI.ViewModel
             Friend = await _dataService.GetByIdAsync(friendId);
         }
 
-        public Friend Friend
-        {
-            get { return _friend; }
-            private set
-            {
-                _friend = value;
-                OnPropertyChanged();
-            }
-        }
     }
 }
 
---
+++ FriendOrganizer.UI/ViewModel/NavigationItemViewModel.cs
@@ -0,0 +1,25 @@
+﻿namespace FriendOrganizer.UI.ViewModel
+{
+    public class NavigationItemViewModel : ViewModelBase
+    {
+        public int Id { get; }
+
+        private string _displayMember;
+
+        public NavigationItemViewModel(int id, string displayMember)
+        {
+            Id = id;
+            DisplayMember = displayMember;
+        }
+
+        public string DisplayMember
+        {
+            get { return _displayMember; }
+            set
+            {
+                _displayMember = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+}
